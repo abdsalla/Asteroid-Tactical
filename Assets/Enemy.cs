93111919aa1c2a83973af547f94558abba67ae28
abdsalla <35743457+abdsalla@users.noreply.github.com>
2018-05-30 13:29:15 -0700

    Started Project 2

---
+++ Assets/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameMananger.instance.enemies.Add(this); // add enemy to list on start of game
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDestroy () //function for what happens when an enemy is destroyed
+	{
+		GameMananger.instance.enemies.Remove(this);
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private Transform transform;
+	public float movespeed;
+	public float turnspeed;
+
+	// Use this for initialization
+	void Start () {
+		transform = GetComponent<Transform>();
+		GameMananger.instance.player = this; // everything can find the player through the GameManager
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position = transform.position + (Vector3 * movespeed);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate = transform.Rotate + (Vector3 * turnspeed);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate = transform.Rotate + (Vector3 * turnspeed);
+		}
+	}
+}
